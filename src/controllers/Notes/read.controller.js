f431d2a539b0eb2647e6bb8dea6676bdaa365c79
Mustak sk <91077142+Mustak9009@users.noreply.github.com>
2024-07-18 20:31:35 +0530

    Read Note - CRUD(R)

---
+++ src/controllers/Notes/read.controller.js
@@ -0,0 +1,18 @@
+import { Note } from "../../modules/notes.module.js";
+import { asynHandler } from "../../util/asynHandler.js";
+import { ApiResponse } from "../../util/apiResponse.js";
+import { ApiError } from "../../util/apiError.js";
+
+export const readTodo = asynHandler(async (req, res) => {
+  const { userId } = req.body; //TODO: Change According token
+
+  if (!userId) {
+    //For safety
+    throw new ApiError(400, "User ID is required", ["Something went wrong ..!!"]);
+  }
+  const notes = await Note.find({ createdBy: userId });
+
+  return res
+    .status(200)
+    .json(new ApiResponse(200, { notes }, "All notes  have been fetched successfully"));
+});
--- src/routes/note.routes.js
+++ src/routes/note.routes.js
@@ -1,8 +1,10 @@
 import express from "express";
 import { createNote } from "../controllers/Notes/create.controller.js";
+import { readTodo } from "../controllers/Notes/read.controller.js";
 
 const router = express.Router();
 
 router.route("/create").post(createNote);
+router.route("/notes").get(readTodo);
 
 export default router;
