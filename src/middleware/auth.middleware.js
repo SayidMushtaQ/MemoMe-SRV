71cb5086eec88f06046fd965f507bc9a6a2cc46b
Mustak sk <91077142+Mustak9009@users.noreply.github.com>
2024-08-10 08:42:23 +0530

    add with cookies

--- src/middleware/auth.middleware.js
+++ src/middleware/auth.middleware.js
@@ -6,12 +6,7 @@ import { excludedAuthPaths } from "../constants.js";
 export const requreAuthentication = asynHandler(async (req, res, next) => {
   const userToken = new AuthToken();
   if (excludedAuthPaths.includes(req.path)) return next();
-  const tokenHeader = req.headers["authorization"];
-  if (!tokenHeader) {
-    throw new ApiError(401, "Authorization header missing", ["Unauthorized"]);
-  }
-  const token = tokenHeader.split(" ")[1];
-  console.log(token);
+  const token = req.cookies?.authToken;
   req.user = null;
   if (!token)
     throw new ApiError(
