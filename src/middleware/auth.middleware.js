ce23c6f110e50e190bbe501f39da42db80fc202c
Mustak sk <91077142+Mustak9009@users.noreply.github.com>
2024-08-11 10:16:44 +0530

    add

--- src/middleware/auth.middleware.js
+++ src/middleware/auth.middleware.js
@@ -4,22 +4,22 @@ import { ApiError } from "../util/apiError.js";
 import { excludedAuthPaths } from "../constants.js";
 
 export const requreAuthentication = asynHandler(async (req, res, next) => {
-  const userToken = new AuthToken();
-  if (excludedAuthPaths.includes(req.path)) return next();
-  const token = req.cookies?.authToken;
-  console.log(token);
-  req.user = null;
-  if (!token)
-    throw new ApiError(
-      401,
-      "Access denied. No token provided. Please log in to obtain a token.",
-      ["Unauthorized"]
-    );
-  const localUser = userToken.getUser(token);
-  if (!localUser)
-    throw new ApiError(401, "Access denied. Please log in to continue.", [
-      "Unauthorized"
-    ]);
-  req.user = localUser;
+  // const userToken = new AuthToken();
   return next();
+  // if (excludedAuthPaths.includes(req.path)) return next();
+  // const token = req.cookies?.authToken;
+  // req.user = null;
+  // if (!token)
+  //   throw new ApiError(
+  //     401,
+  //     "Access denied. No token provided. Please log in to obtain a token.",
+  //     ["Unauthorized"]
+  //   );
+  // const localUser = userToken.getUser(token);
+  // if (!localUser)
+  //   throw new ApiError(401, "Access denied. Please log in to continue.", [
+  //     "Unauthorized"
+  //   ]);
+  // req.user = localUser;
+  // return next();
 });
