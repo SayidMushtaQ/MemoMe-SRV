fa1d8357cc9c0a4183c3170cbde074dd688d6cfa
Mustak sk <91077142+Mustak9009@users.noreply.github.com>
2024-08-11 10:09:28 +0530

    add

--- src/middleware/auth.middleware.js
+++ src/middleware/auth.middleware.js
@@ -8,7 +8,6 @@ export const requreAuthentication = asynHandler(async (req, res, next) => {
   if (excludedAuthPaths.includes(req.path)) return next();
   const authHeader = req.headers["authorization"];
   const token = authHeader && authHeader.split(" ")[1];
-  console.log(token, authHeader);
   req.user = null;
   if (!token)
     throw new ApiError(
