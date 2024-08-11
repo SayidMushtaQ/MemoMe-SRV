ee4e9f6b31d294821fae6cd44b681ae00d2655c7
Mustak sk <91077142+Mustak9009@users.noreply.github.com>
2024-08-11 14:52:56 +0530

    add

--- src/app.js
+++ src/app.js
@@ -7,7 +7,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: [process.env.CORS_ORIGIN, "http://localhost:5173"],
+    origin: "https://memome-cli.onrender.com",
     credentials: true
   })
 );
