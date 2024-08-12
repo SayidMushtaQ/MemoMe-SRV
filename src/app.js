cafe40a1d157a7bd7a8c571e4965ca3910e19aaa
Mustak sk <91077142+Mustak9009@users.noreply.github.com>
2024-08-12 08:13:53 +0530

    add

--- src/app.js
+++ src/app.js
@@ -2,19 +2,22 @@ import express from "express";
 import { DATA_LIMIT, API_URL } from "./constants.js";
 import cookieParser from "cookie-parser";
 import cors from "cors";
-
+import { fileURLToPath } from "url";
+import path from "path";
 const app = express();
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 app.use(
   cors({
-    origin: ["https://memome-cli.onrender.com", "http://localhost:5173"],
+    origin: process.env.CORS_ORIGIN,
     credentials: true
   })
 );
 
 app.use(express.json({ limit: DATA_LIMIT }));
 app.use(express.urlencoded({ extended: true, limit: DATA_LIMIT }));
-app.use(express.static("public"));
+app.use(express.static(path.join(__dirname, "..", "public")));
 app.use(cookieParser(process.env.COOKIE_PARSER_SECRET));
 app.use(requreAuthentication);
 
@@ -22,6 +25,9 @@ import userRouter from "./routes/user.routes.js";
 import noteRoter from "./routes/note.routes.js";
 import { requreAuthentication } from "./middleware/auth.middleware.js";
 
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "..", "public", "index.html"));
+});
 app.use(`${API_URL}/auth`, userRouter);
 app.use(`${API_URL}/note`, noteRoter);
 
